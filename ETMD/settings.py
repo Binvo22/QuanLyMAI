14956da365ea4fab3cf5ba8d845e5d6e9cd19710
Binvo22 <truongngoclinhnhi12102003@gmail.com>
2025-06-16 17:16:16 +0700

    Fix health check path for Render

--- ETMD/settings.py
+++ ETMD/settings.py
@@ -87,14 +87,15 @@ WSGI_APPLICATION = "ETMD.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'employee',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': '220903',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': 'msSkrNnDQTKppwZeHGGPbuvwLAMABzQP',
+        'HOST': 'shortline.proxy.rlwy.net',
+        'PORT': '33716',
     }
 }
 
